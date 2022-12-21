175fe86fb8efd08db1a4c86dbe54b8e730fdeb85
Eg0rR0di0n0v <Eg0rR0di0n0v@yandex.ru>
2022-12-21 16:58:47 +0300

    compled

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,28 @@ public class Main {
             );
         }
 
+        Long peopleUpTo18 = persons.stream()
+                .filter(person -> person.getAge() < 18)
+                .count();
+        System.out.printf("Number of minor residents of Silent hill - %,d.%n", peopleUpTo18);
+
+        List<String> recruits = persons.stream()
+                .filter(person -> person.getAge() >= 18 && person.getAge() <= 27)
+                .map(person -> person.getFamily()).toList();
+        System.out.printf("Number of conscripts on the list - %,d.%n", recruits.size());
+
+        List<Person> humanResources = persons.stream()
+                .filter(person -> person.getEducation().equals(Education.HIGHER))
+                .filter(person -> {
+                    if (person.getSex() == Sex.MAN) {
+                        return person.getAge() >= 18 && person.getAge() <= 65;
+                    } else {
+                        return person.getAge() >= 18 && person.getAge() <= 60;
+                    }
+                })
+                .sorted(Comparator.comparing(Person::getFamily)).toList();
+        System.out.printf("The number of able-bodied citizens with higher" +
+                "education according to the list - %,d.%n", humanResources.size());
+
     }
 }
